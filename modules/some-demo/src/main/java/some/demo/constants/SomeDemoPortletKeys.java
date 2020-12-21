aca62c6eede0f3038ea8f6eecfe246335af6b839
aroraapankaj123 <aroraapankaj123@gmail.com>
2020-12-21 11:03:58 +0530

    added demo workspace to git

---
+++ modules/some-demo/src/main/java/some/demo/constants/SomeDemoPortletKeys.java
@@ -0,0 +1,11 @@
+package some.demo.constants;
+
+/**
+ * @author liferay
+ */
+public class SomeDemoPortletKeys {
+
+	public static final String SOMEDEMO =
+		"some_demo_SomeDemoPortlet";
+
+}
---
+++ modules/some-demo/src/main/java/some/demo/portlet/SomeDemoPortlet.java
@@ -0,0 +1,30 @@
+package some.demo.portlet;
+
+import some.demo.constants.SomeDemoPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author liferay
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=SomeDemo",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + SomeDemoPortletKeys.SOMEDEMO,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class SomeDemoPortlet extends MVCPortlet {
+}
